44dcf6c62d5d8e2ff5cd312326cc466a86f84d1a
oitsjustjose <stovallj1995@gmail.com>
2019-09-23 16:09:14 -0400

    Initial commit

---
+++ how-to.r
@@ -0,0 +1,44 @@
+#############################################
+#         Basic dataset fun and             #
+#            Initialization                 #
+#############################################
+
+# Read in and print off the columns of the dataset
+dataset <- read.csv("mlk-georgia-cam-1-2019-3.csv")
+colnames(dataset)
+
+# To refer to a column in a dataset, use the '$' selector:
+head(dataset$hit_counts)
+
+# Prune the locations column because it isn't statistically useful.
+# Print off the columns again to show the change
+dataset = dataset[, !(names(dataset) %in% c("locations"))]
+colnames(dataset)
+# Plot the whole dataset just for fun
+plot(dataset)
+# Create a subset of the dataset containing only the label and timestamp columns
+barplotSubset <- subset(dataset, select = c("label", "timestamp"))
+# Create a barplot of the subset
+barplot(table(toHistogram))
+
+#############################################
+#    Now, let's have look into some stats   #
+#  Let's find the mean, median and mode of  #
+#    The hit_counts column of the dataset   #
+#############################################
+
+mean(dataset$hit_counts)
+median(dataset$hit_counts)
+
+#############################################
+# Unfortunately R doens't offer a built-in  #
+#  "mode" function - so instead we'll make  #
+#   our own using a function declaration:   #
+#############################################
+
+ourMode <- function(v) {
+  uniqv <- unique(v)
+  uniqv[which.max(tabulate(match(v, uniqv)))]
+}
+
+ourMode(dataset$hit_counts)
---
+++ tasks.md
@@ -0,0 +1,21 @@
+# R Statistics Assignment
+
+
+
+##Your Tasks
+
+1. Install R on your machine
+2. Run the demo code (`how-to.r`) in R using the command `r -f how-to.r` or [R-Studio](https://www.rstudio.com/).
+3. Write a new file named `tasks.r` which:
+   1. Reads in the dataset (exactly like in `how-to.r`)
+   2. Finds the variance of the `hit_counts` column
+   3. Finds the standard deviation of the `hit_counts` column. Standard Deviation is equal to $\sqrt{variance}$ , so you can find this two ways:
+      1. Find the square root of the variance from step 2
+      2. Use the built-in R command
+   4. Find the frequency of each type of label from the `label` column -- you **will not** and **should not** need to install a library to do this!
+
+
+
+### You may need to use Google for these!
+
+If so, that's perfectly fine! That's what we do as "professional" programmers, so we understand that not everything is straight forward
