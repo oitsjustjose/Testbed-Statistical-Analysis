1dfb0d9318bb0ddded29b24e706cea1276cb5f29
oitsjustjose <stovallj1995@gmail.com>
2019-09-23 16:19:05 -0400

    Plot to JPGs

--- how-to.r
+++ how-to.r
@@ -14,12 +14,19 @@ head(dataset$hit_counts)
 # Print off the columns again to show the change
 dataset = dataset[, !(names(dataset) %in% c("locations"))]
 colnames(dataset)
-# Plot the whole dataset just for fun
+
+# Plot the whole dataset just for fun, save it to dataset-plot.jpg
+jpeg("dataset-plot.jpg", width = 1000, height = 1000)
 plot(dataset)
+dev.off()
+
 # Create a subset of the dataset containing only the label and timestamp columns
 barplotSubset <- subset(dataset, select = c("label", "timestamp"))
-# Create a barplot of the subset
+
+# Create a barplot of the subset, save it to dataset-barplot.jpeg
+jpeg("dataset-barplot.jpg", width=1000, height=1000)
 barplot(table(barplotSubset))
+dev.off()
 
 #############################################
 #    Now, let's have look into some stats   #
